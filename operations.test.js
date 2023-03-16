ba06267b5dc36fbbb988aa3bbad8a59be140f2b1
Ricardo Rodriguez <ricardorodriguez789@u.boisestate.edu>
2023-03-16 13:42:13 -0600

    Closes #1

--- operations.test.js
+++ operations.test.js
@@ -1,8 +1,20 @@
 // "import" the sum function from operations.js
 const {sum} = require('./operations')
 
-//TODO: uncomment the next line
-//const {subtract} = require('./operations')
+//TODO: uncomment the next line (Done)
+const {subtract} = require('./operations')
+
+test('subtracts 3 - 1 to equal 2', () => {
+    expect(subtract(3, 1)).toBe(2)
+});
+
+test('subtracts 3 - 7 to equal -4', () => {
+    expect(subtract(3, 7)).toBe(-4)
+});
+
+test('subtracts 0 - (-5) to equal 5', () => {
+    expect(subtract(0, -5)).toBe(5)
+});
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
